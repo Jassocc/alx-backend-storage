e33a34474487f8cd98df907f8562f939d829090b
Jassocc <clnearle@gmail.com>
2024-04-29 01:26:36 -0700

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+task 12 script
+"""
+from pymongo import MongoClient
+
+
+def print_request_logs(nginx_collection):
+    """
+    prints nginx logs
+    """
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods')
+    metho = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in metho:
+        req_c = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_c))
+    stat_check = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(stat_check))
+
+
+def run():
+    """
+    gives some stats
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_request_logs(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    run()
